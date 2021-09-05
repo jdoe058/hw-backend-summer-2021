d9611828fe43635a669b2fbeecc953ffc1abbcd9
Zheka Doe <jdoe058@gmail.com>
2021-09-05 19:11:02 +0300

    I want to sleep

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
 
 from app.store.database.gino import db
 
@@ -9,13 +9,12 @@ class Theme:
     id: Optional[int]
     title: str
 
-# TODO
-# Дописать все необходимые поля модели
+
 class ThemeModel(db.Model):
     __tablename__ = "themes"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, unique=True, nullable=False)
 
 
 # TODO
@@ -24,7 +23,9 @@ class AnswerModel(db.Model):
     __tablename__ = "answers"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, unique=True, nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.id'))
 
 
 @dataclass
@@ -34,13 +35,29 @@ class Question:
     theme_id: int
     answers: list["Answer"]
 
+
 # TODO
 # Дописать все необходимые поля модели
 class QuestionModel(db.Model):
     __tablename__ = "questions"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, unique=True)
+    theme_id = db.Column(db.Integer, db.ForeignKey('themes.id'), nullable=False)
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+        self._answers: List[AnswerModel] = list()
+
+    @property
+    def answers(self) -> List[AnswerModel]:
+        return self._answers
+
+    @answers.setter
+    def answers(self, val: Optional[AnswerModel]):
+        if val is not None:
+            self._answers.append(val)
 
 
 @dataclass

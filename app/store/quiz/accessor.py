ff938c3d30278159fdf9ffbfb497b8fa93173ede
Zheka Doe <jdoe058@gmail.com>
2021-09-05 13:00:44 +0300

    ThemeModel released without test_create_question_unique_title_constraint

--- app/store/quiz/accessor.py
+++ app/store/quiz/accessor.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from aiohttp.web_exceptions import HTTPException
+
 from app.base.base_accessor import BaseAccessor
 from app.quiz.models import (
     Theme,
@@ -14,16 +16,21 @@ from typing import List
 
 class QuizAccessor(BaseAccessor):
     async def create_theme(self, title: str) -> Theme:
-        raise NotImplementedError
+        res = await ThemeModel.create(title=title)
+        return Theme(**res.to_dict())
+
 
     async def get_theme_by_title(self, title: str) -> Optional[Theme]:
-        raise NotImplementedError
+        res = await ThemeModel.query.where(ThemeModel.title == title).gino.first()
+        return Theme(**res.to_dict()) if res else None
 
     async def get_theme_by_id(self, id_: int) -> Optional[Theme]:
-        raise NotImplementedError
+        res = await ThemeModel.query.where(ThemeModel.id == id_).gino.first()
+        return Theme(**res.to_dict()) if res else None
 
     async def list_themes(self) -> List[Theme]:
-        raise NotImplementedError
+        res = await ThemeModel.query.gino.all()
+        return [Theme(**r.to_dict()) for r in res]
 
     async def create_answers(self, question_id, answers: List[Answer]):
         raise NotImplementedError
